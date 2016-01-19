cc709ce8639f19cca8cef53666ecfc8770cabe73
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-01-19 10:56:09 -0500

    Add db to default handler

--- feasted-api/handler.py
+++ feasted-api/handler.py
@@ -3,10 +3,13 @@ from tornado.web import Finish, RequestHandler
 
 class DefaultHandler(RequestHandler):
     
+    @property
+    def db_conn(self):
+        return self.application.db_conn
+    
     def delete(self, *args, **kwargs):
         self.set_status(405)
         raise Finish()
-
 
     def get(self, *args, **kwargs):
         self.set_status(405)
@@ -16,21 +19,17 @@ class DefaultHandler(RequestHandler):
         self.set_status(405)
         raise Finish()
 
-    
     def options(self, *args, **kwargs):
         self.set_status(405)
         raise Finish()
-
 
     def patch(self, *args, **kwargs):
         self.set_status(405)
         raise Finish()
 
-
     def post(self, *args, **kwargs):
         self.set_status(405)
         raise Finish()
-
 
     def put(self, *args, **kwargs):
         self.set_status(405)
