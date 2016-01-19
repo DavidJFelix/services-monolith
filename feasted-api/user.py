73a936617ccb8bcfb5d574f4409d58652d5dd242
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-01-19 14:06:31 -0500

    Add functions for user

--- feasted-api/user.py
+++ feasted-api/user.py
@@ -11,7 +11,6 @@ class UserHandler(DefaultHandler):
         # Get the user from the header and mark inactive
         raise Finish()
 
-    
     @gen.coroutine
     def get(self):
         user_id = yield self.check_auth_for_user_id()
@@ -25,23 +24,46 @@ class UserHandler(DefaultHandler):
             self.write({})
         raise Finish()
 
-
     def head(self):
         # TODO: extract get and implement head from extracted
         raise Finish()
 
-
     def options(self):
         raise Finish()
         
-
+    @gen.coroutine
     def patch(self):
+        # TODO: extract parts to generic update function
+        user_id = yield self.check_auth_for_user_id()
+        user = self.validate_json_for_user(self.request.body)
+        resp = yield rdb.table("users").\
+            update(user, durability='hard', return_changes=True).\
+            run(self.db_conn)
+        if resp.get('replaced', 0) == 1:
+            self.set_status(200)
+            self.write(resp.get('changes', {}).get('new_val', {}))
+        else:
+            # TODO: extract to a 500 error
+            self.set_status(500)
+            self.write({})
         raise Finish()
-    
     
     def post(self):
         raise Finish()
 
-
+    @gen.coroutine
     def put(self):
+        # TODO: extract parts to generic update function
+        user_id = yield self.check_auth_for_user_id()
+        user = self.validate_json_for_user(self.request.body)
+        resp = yield rdb.table("users").\
+            update(user, durability='hard', return_changes=True).\
+            run(self.db_conn)
+        if resp.get('replaced', 0) == 1:
+            self.set_status(200)
+            self.write(resp.get('changes', {}).get('new_val', {}))
+        else:
+            # TODO: extract to a 500 error
+            self.set_status(500)
+            self.write({})
         raise Finish()
