b733b5d27d4ed704cdfdfb678e85cdec9ac0b03d
foobaar <baargavrag@gmail.com>
2016-02-18 23:49:00 -0500

    added rudimentary excpetion handling

--- feasted_api/handlers/allergens.py
+++ feasted_api/handlers/allergens.py
@@ -7,8 +7,12 @@ from tornado.web import Finish, HTTPError
 
 @gen.coroutine
 def get_allergens(db_conn):
-    allergens = yield rdb.table('allergens').order_by('allergen').run(db_conn)
-    return allergens
+    try:
+        allergens = yield rdb.table('allergenss').order_by('allergen').run(db_conn)
+        return allergens
+    except rdb.ReqlRuntimeError:
+        print('error reading from table')
+        return "[]"
 
 
 from .base import DefaultHandler
--- feasted_api/handlers/ingredients.py
+++ feasted_api/handlers/ingredients.py
@@ -7,9 +7,12 @@ from tornado.web import Finish, HTTPError
 
 @gen.coroutine
 def get_ingredients(db_conn):
-    ingredients = yield rdb.table('ingredients').order_by('ingredient').run(db_conn)
-    return ingredients
-
+    try:
+        ingredients = yield rdb.table('ingredients').order_by('ingredient').run(db_conn)
+        return ingredients
+    except rdb.RqlRuntimeError:
+        print('error reading from table')
+        return "[]"
 
 from .base import DefaultHandler
 
