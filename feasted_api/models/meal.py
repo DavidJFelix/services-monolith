1a618efbfe70bb32089e5bb4e5aa8ae2b534f8c9
David J Felix <felix.davidj@gmail.com>
2016-02-23 21:11:23 -0500

    Unwrap object from rethink meals

--- feasted_api/models/meal.py
+++ feasted_api/models/meal.py
@@ -27,9 +27,32 @@ Location = namedtuple('Location', [
 @gen.coroutine
 def get_meals(ll, radius, max_results, db_conn):
     meals_nearby = yield rdb.table('meals').get_nearest(ll, index='location', max_dist=radius,
-                                                        max_results=max_results).run(db_conn)
+                                                        max_results=max_results,
+                                                        unit='mi').run(db_conn)
+    # FIXME: getttoooo powers activate
+    meals = []
+    for meal_obj in meals_nearby:
+        meal = Meal()
+        meal.meal_id = meal_obj['doc']['id']
+        meal.name = meal_obj['doc']['name']
+        meal.description = meal_obj['doc']['description']
+        meal.portions = meal_obj['doc']['portions']
+        meal.price = meal_obj['doc']['price']
+        meal.availableFrom = meal_obj['doc']['availableFrom']
+        meal.availableTo = meal_obj['doc']['availableTo']
+        meal.ingredients = meal_obj['doc']['ingredients']
+        meal.allergens = meal_obj['doc']['allergens']
+        meal.imageUrl = meal_obj['doc']['image_url']
+        meal.isActive = meal_obj['doc']['isActive']
+        location = Location()
+        location.type = meal_obj['doc']['location']['type']
+        location.coordinates = {
+            "lng": meal_obj['doc']['location']['coordinates'][0],
+            "lat": meal_obj['doc']['location']['coordinates'][1]
+        }
+        meal.location = location._asdict()
     return {
-        "meals": meals_nearby
+        "meals": meals
     }
 
 
