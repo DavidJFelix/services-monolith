18ce4823e6a9b793469abf2f39e03075fa15d54b
David J Felix <felix.davidj@gmail.com>
2016-02-25 10:21:05 -0500

    Improve consistency and add some comments

--- feasted_api/handlers/base.py
+++ feasted_api/handlers/base.py
@@ -6,6 +6,12 @@ from tornado.web import RequestHandler, HTTPError
 
 
 class DefaultHandler(RequestHandler):
+    """ DefaultHandler is a base handler that all of our handlers will inherit from.
+
+    It sets up our desired error format, and a default policy for method response when handlers
+    are hit with a method that they do not support.
+    """
+
     def data_received(self, chunk):
         # This is just here to satisfy pycharm
         super().data_received(chunk)
@@ -18,7 +24,7 @@ class DefaultHandler(RequestHandler):
             if isinstance(exception, HTTPError) and exception.reason:
                 reason = exception.reason
             else:
-                reason = "Unexpected runtime exception"
+                reason = "unexpected runtime exception"
 
         self.write({
             "error": status_code,
@@ -31,28 +37,33 @@ class DefaultHandler(RequestHandler):
         return conn
 
     def delete(self, *args, **kwargs):
-        raise HTTPError(405, reason="Method not allowed.")
+        raise HTTPError(405, reason="method not allowed")
 
     def get(self, *args, **kwargs):
-        raise HTTPError(405, reason="Method not allowed.")
+        raise HTTPError(405, reason="method not allowed")
 
     def head(self, *args, **kwargs):
-        raise HTTPError(405, reason="Method not allowed.")
+        raise HTTPError(405, reason="method not allowed")
 
     def options(self, *args, **kwargs):
-        raise HTTPError(405, reason="Method not allowed.")
+        raise HTTPError(405, reason="method not allowed")
 
     def patch(self, *args, **kwargs):
-        raise HTTPError(405, reason="Method not allowed.")
+        raise HTTPError(405, reason="method not allowed")
 
     def post(self, *args, **kwargs):
-        raise HTTPError(405, reason="Method not allowed.")
+        raise HTTPError(405, reason="method not allowed")
 
     def put(self, *args, **kwargs):
-        raise HTTPError(405, reason="Method not allowed.")
+        raise HTTPError(405, reason="method not allowed")
 
 
 class BaseBearerAuthHandler(DefaultHandler):
+    """BaseBearerAuthHandler is an extension of Default handler which is used when a handler needs
+    to tie into the auth system.
+
+    It provides utility functions to work with and validate auth headers and tokens.
+    """
     def get_bearer_token(self) -> Optional[str]:
         auth_header = self.request.headers.get('authorization', None)
         if auth_header:
