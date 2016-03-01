d30a71f919b17ff64e1fbea3e1b1634f0ab5d7b4
David J Felix <felix.davidj@gmail.com>
2016-03-01 14:56:18 -0500

    Cut out the validation

--- feasted_api/models/meal.py
+++ feasted_api/models/meal.py
@@ -1,36 +1,26 @@
-from .allergen import Allergen
 from .base import (
     BaseCollectionModel,
     BaseModel,
-    BooleanValue,
-    DateTimeValue,
-    IntegerValue,
-    PriceValue,
-    StringValue,
-    URLValue,
-    UUIDValue,
 )
-from .ingredient import Ingredient
-from .location import Location
 
 
 class Meal(BaseModel):
-    fields = {
-        'meal_id': UUIDValue,
-        'name': StringValue,
-        'description': StringValue,
-        'portions': IntegerValue,
-        'portions_available': IntegerValue,
-        'price': PriceValue,
-        'location': Location,
-        'available_from': DateTimeValue,
-        'available_to': DateTimeValue,
-        'ingredients': [Ingredient],
-        'allergens': [Allergen],
-        'preview_image_url': URLValue,
-        'images': [URLValue],
-        'is_active': BooleanValue,
-    }
+    fields = (
+        'meal_id',
+        'name',
+        'description',
+        'portions',
+        'portions_available',
+        'price',
+        'location',
+        'available_from',
+        'available_to',
+        'ingredients',
+        'allergens',
+        'preview_image_url',
+        'images',
+        'is_active',
+    )
     rethink_table = 'meals'
     id_field = 'meal_id'
 
